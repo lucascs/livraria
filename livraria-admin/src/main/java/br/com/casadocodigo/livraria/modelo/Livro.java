875ef5a529f688efabb7c1564ef20ce483938c0f
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2013-05-25 12:22:25 -0300

    Criando o modelo Livro

---
+++ livraria-admin/src/main/java/br/com/casadocodigo/livraria/modelo/Livro.java
@@ -0,0 +1,52 @@
+package br.com.casadocodigo.livraria.modelo;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+public class Livro {
+	private String titulo;
+	private String descricao;
+	private String isbn;
+	private BigDecimal preco;
+	private Calendar dataPublicacao;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Calendar getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public void setDataPublicacao(Calendar dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+}
