d4eb2f34b86e7e91130f6f2415ddea9e383a0e74
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2013-06-25 08:48:59 -0300

    Modificando o enum Moeda para incluir o símbolo

--- livraria-admin/src/main/java/br/com/casadocodigo/livraria/modelo/Moeda.java
+++ livraria-admin/src/main/java/br/com/casadocodigo/livraria/modelo/Moeda.java
@@ -1,5 +1,16 @@
 package br.com.casadocodigo.livraria.modelo;
 
 public enum Moeda {
-	REAL, DOLAR, EURO, LIBRA
+	REAL("R$"), DOLAR("US$"),
+	EURO("€"), LIBRA("£");
+
+	private final String simbolo;
+
+	private Moeda(String simbolo) {
+		this.simbolo = simbolo;
+	}
+
+	public String getSimbolo() {
+		return simbolo;
+	}
 }
